ebeeb89f3797d2eb7cd938bfbef0782b888cd9d2
mike <mikecee0815@gmail.com>
2021-05-19 11:52:48 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Test from './components/test/test';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Test />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/test/test.js
@@ -0,0 +1,10 @@
+
+const Test = () => {
+  return (
+    <div>
+      <p>testing.....</p>
+    </div>
+  );
+}
+
+export default Test;
