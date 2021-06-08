81357b54c0d0ab05d319e44d4963dc5f3c252219
mike <mikecee0815@gmail.com>
2021-06-07 23:02:48 -0400

    company component styling

--- src/components/company/Company.js
+++ src/components/company/Company.js
@@ -1,4 +1,10 @@
 import React, { useState,useEffect } from  'react'
+import './style.css'
+
+
+const styles = {
+  fontSize:'18',
+}
 
 const Company = () => {
 
@@ -21,10 +27,17 @@ const Company = () => {
     },[])
 
   return (
-    <div className="App">
-      <h3>Company:{company.name}</h3>
-      <h3>Ceo:{company.ceo}</h3>
-      <p>{company.summary}</p>
+    <div className='wrapper'>
+    
+      <h1>{company.name}</h1>
+      <div className='ceo'>
+        <h3>Ceo:</h3>
+        <h3>{company.ceo}</h3>
+      </div>
+      
+      <div className='summary'>
+        <p>{company.summary}</p>
+      </div> 
     </div>
   );
 }
