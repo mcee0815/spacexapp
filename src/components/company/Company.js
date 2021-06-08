415561d04c81537528e3727226a5f96f3337e76d
mike <mikecee0815@gmail.com>
2021-06-07 23:10:24 -0400

    styled company-info

--- src/components/company/Company.js
+++ src/components/company/Company.js
@@ -1,15 +1,6 @@
 import React, { useState,useEffect } from  'react'
-<<<<<<< HEAD
 import ClipLoader from "react-spinners/ClipLoader";
 import MoonLoader from "react-spinners/MoonLoader";
-=======
-import './style.css'
-
-
-const styles = {
-  fontSize:'18',
-}
->>>>>>> company-info
 
 const Company = () => {
 
@@ -37,7 +28,6 @@ const Company = () => {
     },[])
 
   return (
-<<<<<<< HEAD
     <div className="App">
     {
       spin ? <ClipLoader/> :
@@ -48,19 +38,6 @@ const Company = () => {
       </div>
       
     }
-=======
-    <div className='wrapper'>
-    
-      <h1>{company.name}</h1>
-      <div className='ceo'>
-        <h3>Ceo:</h3>
-        <h3>{company.ceo}</h3>
-      </div>
-      
-      <div className='summary'>
-        <p>{company.summary}</p>
-      </div> 
->>>>>>> company-info
     </div>
   );
 }
